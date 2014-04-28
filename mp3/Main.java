251d726855b74041786fb46392e3285d03d4b1b3
Read Sprabery <read.sprabery@gmail.com>
2014-04-27 21:21:05 -0500

    Added encrypt and decryption
    Examples are in the main file, but MP3Encryption can be imported and
    used as is.

---
+++ mp3/Main.java
@@ -0,0 +1,11 @@
+package com.mp3;
+
+import com.mp3.MP3Encryption;
+public class Main {
+
+    public static void main(String[] args) {
+        MP3Encryption enc = new MP3Encryption("illinois");
+        System.out.println(enc.encrypt("abcde"));
+        System.out.println(enc.decrypt("E74C16F61583C2D93F33B6F0E2616627"));
+    }
+}
