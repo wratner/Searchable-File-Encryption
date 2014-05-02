eaf0f5e832486d6f407cfad07028be82a6a51a78
read <read.sprabery@gmail.com>
2014-05-01 19:02:40 -0500

    Added scripts to compile and run script

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #make any changes need to run the server (including passing the arguments to the encrypted storage location or any other parameters)
+
+java -cp `pwd` BlindStorageServer
