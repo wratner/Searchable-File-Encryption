76ea5eabb0e55ab6fb75793a2c6aae675ca47252
Govind Raghunath <govindr@ubuntu.(none)>
2014-05-01 17:51:07 -0700

    Client Lookup stuff.

--- ClientActivity.java
+++ ClientActivity.java
@@ -32,21 +32,74 @@ public class ClientActivity extends Activity {
 												 */
 	public static final String LOOKUP_CMD = "LOOKUP ";
 
+	// For Part-1
+	// USE Local Index (cached) and find the documentId from there
+	// For Part-2
+	// (a) if index is present in local cache use it to find the document id
+	// (otherwise) use the download functionality to get the appropriate
+	// document-ids from the server
+	// and use them to download the files: This would be done by
+	// implementing SSE.Search functionality
 	public static void Lookup(String keyword, PrintWriter output) {
-		String documentId = "";
+		FileReader fileRead;
 
-		// For Part-1
-		// USE Local Index (cached) and find the documentId from there
-		// For Part-2
-		// (a) if index is present in local cache use it to find the document id
-		// (otherwise) use the download functionality to get the appropriate
-		// document-ids from the server
-		// and use them to download the files: This would be done by
-		// implementing SSE.Search functionality
+		String documentId;
+		String currString;
+		int nextChar;
+		String currKeyword;
 
-		//PrintWriter.write("LOOKUP " + documentId); // write the message to
-													// output stream
+		fileRead = new FileReader(indexPath);
+
+		documentId = "";
+		currKeyword = "";
+		currString = "";
+		nextChar = fileRead.read();
+		while ((nextChar != -1) && (documentId == "")) {
+			if (((char) nextChar == KEYWORD_DELIM) || ((char) nextChar == MAPPING_DELIM)) {
+				if (currKeyword == "") {
+					currKeyword = currString;
+					currString = "";
+				} else if (currKeyword == keyword) {
+					documentId = currString;
+				}
+
+				if ((char) nextChar == MAPPING_DELIM) {
+					currKeyword = "";
+				}
+			} else {
+				currString += (char) nextChar;
+			}
+			nextChar = fileRead.read();
+		}
+
+		fileRead.close();
+
+		output.write("LOOKUP " + documentId); // write the message to
+
 		// Get the output and show to the user
+	}
+
+	/*
+	 * Parses a mapping of keywords to message ID's from a file.
+	 *
+	 * Parameters:
+	 * indexPath: The filepath for the index file.
+	 *
+	 * Returns:
+	 * The parsed mapping. If there is a failure in execution, null is returned.
+	 */
+	public static Set<String> indexFileToMap(String indexPath, String keyword) throws IOException {
+		Map<String, Set<String>> map;
+
+		map = new HashMap<String, Set<String>>();
+
+
+		currString = "";
+		currKeyword = "";
+		currMessages = new HashSet<String>();
+
+
+		return map;
 	}
 
 	@Override
