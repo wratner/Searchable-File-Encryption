92d68de2633704033c16508ff9a0a7200ff2a079
Read Sprabery <read.sprabery@gmail.com>
2014-05-11 00:09:13 -0500

    Function to create blind store from files

--- src/mp3/CreateBlindStore.java
+++ src/mp3/CreateBlindStore.java
@@ -1,7 +1,39 @@
 package mp3;
 
+import java.io.File;
+
 /**
  * Created by Read on 5/10/2014.
  */
 public class CreateBlindStore {
+    public static void main(String[] args) {
+        BlindStorage storage = new BlindStorage(2048, true, false);
+        File file = new File(args[0]);
+        String[] subDirs = file.list();
+        for (String fileName: subDirs) {
+            if (fileName.equals("keywords")) {
+                String[] keywords = (new File(args[0] + "/" + fileName)).list();
+                for(String keyword: keywords) {
+                    File keyWordFile = new File(args[0] + "/" + fileName + "/" + keyword);
+                    storage.addFile(keyWordFile);
+                }
+            } else {
+                String[] userDirs  = (new File(args[0] + "/" + fileName)).list();
+                for (String user: userDirs) {
+                    String[] emails = (new File(args[0] + '/' + fileName + '/' + user)).list();
+                    for(String emailName: emails) {
+                        if (!(emailName == null)) {
+                            File email = new File(args[0] + '/' + fileName + '/' + user + "/" + emailName);
+                            storage.addFile(email);
+                        }
+                        else {
+                            System.out.println("WTF");
+                        }
+                    }
+                }
+            }
+        }
+        System.out.printf("Files Added: " + storage.getFilesAdded());
+//        storage.addFile()
+    }
 }
