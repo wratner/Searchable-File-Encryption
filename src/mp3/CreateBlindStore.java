2145815391162c61c36a8c4503059a490c280625
Read Sprabery <read.sprabery@gmail.com>
2014-05-11 11:46:44 -0500

    Refactored Add files method

--- src/mp3/CreateBlindStore.java
+++ src/mp3/CreateBlindStore.java
@@ -7,33 +7,38 @@ import java.io.File;
  */
 public class CreateBlindStore {
     public static void main(String[] args) {
+        addFilesFromDirectory(args[0]);
+//        storage.addFile()
+    }
+
+    public static void addFilesFromDirectory(String directory) {
         BlindStorage storage = new BlindStorage(2048, true, false);
-        File file = new File(args[0]);
+        String datasetDirectory = directory;
+        File file = new File( datasetDirectory);
         String[] subDirs = file.list();
         for (String fileName: subDirs) {
             if (fileName.equals("keywords")) {
-                String[] keywords = (new File(args[0] + "/" + fileName)).list();
+                String[] keywords = (new File( datasetDirectory + "/" + fileName)).list();
                 for(String keyword: keywords) {
-                    File keyWordFile = new File(args[0] + "/" + fileName + "/" + keyword);
+                    File keyWordFile = new File( datasetDirectory + "/" + fileName + "/" + keyword);
                     storage.addFile(keyWordFile);
                 }
             } else {
-                String[] userDirs  = (new File(args[0] + "/" + fileName)).list();
+                String[] userDirs  = (new File( datasetDirectory + "/" + fileName)).list();
                 for (String user: userDirs) {
-                    String[] emails = (new File(args[0] + '/' + fileName + '/' + user)).list();
+                    String[] emails = (new File( datasetDirectory + '/' + fileName + '/' + user)).list();
                     for(String emailName: emails) {
                         if (!(emailName == null)) {
-                            File email = new File(args[0] + '/' + fileName + '/' + user + "/" + emailName);
+                            File email = new File( datasetDirectory + '/' + fileName + '/' + user + "/" + emailName);
                             storage.addFile(email);
                         }
                         else {
-                            System.out.println("WTF");
+                            System.out.println("Invalid email");
                         }
                     }
                 }
             }
         }
         System.out.printf("Files Added: " + storage.getFilesAdded());
-//        storage.addFile()
     }
 }
