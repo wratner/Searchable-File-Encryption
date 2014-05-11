dc574c37da904cf17611525fe25b4b182af14396
read <read.sprabery@gmail.com>
2014-05-11 16:23:58 -0500

    Scripts to compile and run server

--- compile_server.sh
+++ compile_server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 #make any changes needed to compile the server
-javac BlindStorageServer.java
+#javac src/mp3/BlindStorageServer.java
+#javac src/mp3/BlindStorage.java -cp src
+#javac -d bin src/mp3/*.java
+#javac -d bin -cp `pwd`/bin src/BlindStorageServer.java
+mkdir bin
+javac -d bin src/mp3/*.java
+javac -d bin -cp `pwd`/bin src/BlindStorageServer.java
--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #make any changes need to run the server (including passing the arguments to the encrypted storage location or any other parameters)
 
-java -cp `pwd` BlindStorageServer
+if [[ `uname` == 'Linux' ]]; then
+  java -cp `pwd`/bin BlindStorageServer `ifconfig eth0 | grep "inet addr" | awk 'sub(/addr:/,""){print $2}'` 
+else 
+  java -cp `pwd`/bin BlindStorageServer `ifconfig en1 | grep "netmask" | awk 'sub(/inet/,""){print $1}'` 
+fi
